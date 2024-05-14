1a3e9df48775057c01c96c1f6b87ea17b879dca3
dongyoung <dong0579@naver.com>
2024-05-14 10:11:51 +0900

    first commit: 프로젝트 생성

---
+++ src/main/java/com/cotato/squadus/SquadusApplication.java
@@ -0,0 +1,13 @@
+package com.cotato.squadus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquadusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cotato/squadus/SquadusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotato.squadus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
