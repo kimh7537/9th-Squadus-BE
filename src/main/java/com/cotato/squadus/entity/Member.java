ce81ede5e71f3b5e1e8f93fadd4e1a3a06e75f6d
dongyoung <goalSetter09@naver.com>
2024-06-28 00:55:09 +0900

    Feat: username, password, role 임시 구현

--- src/main/java/com/cotato/squadus/entity/Member.java
+++ src/main/java/com/cotato/squadus/entity/Member.java
@@ -19,6 +19,13 @@ public class Member {
 
     private String memberId;
 
+    private String username; // 임시로 생성
+
+    private String password; // 임시로 생성
+
+    private String role; // 임시로 생성
+
+
     private String email;
 
     private String profileImage;
@@ -27,6 +34,18 @@ public class Member {
 
     @Enumerated(EnumType.STRING)
     private MemberRole memberRole;
+
+    // jwt refresh token
+    @Column(length = 1000)
+    private String refreshToken;
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public void destroyRefreshToken() {
+        this.refreshToken = null;
+    }
 
     @OneToMany(mappedBy = "member", fetch = LAZY, cascade = ALL)
     private List<ClubMember> clubMemberships; // 가입된 동아리 리스트 확인
