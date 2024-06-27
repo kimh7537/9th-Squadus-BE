19fb4c5db2ed45719a0bb43cf5bedda507605da5
dongyoung <goalSetter09@naver.com>
2024-06-28 01:23:43 +0900

    Feat: JWT 발급 및 검증 클래스 구현

--- src/main/java/com/cotato/squadus/jwt/LoginFilter.java
+++ src/main/java/com/cotato/squadus/jwt/LoginFilter.java
@@ -6,9 +6,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.naming.AuthenticationException;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
