35b2612cdcfe6c4b30f8f06f7a06c4ddd6eedc2d
kimh7537 <kimh7537@gmail.com>
2024-07-01 19:10:55 +0900

    fix: scripts/start-server.sh 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-docker stop cicd-server || true
-docker rm cicd-server || true
-docker pull 992382743841.dkr.ecr.ap-northeast-2.amazonaws.com/cicd-server:latest
-docker run -d --name cicd-server -p 8080:8080 992382743841.dkr.ecr.ap-northeast-2.amazonaws.com/cicd-server:latest
+docker stop squadus-server || true
+docker rm squadus-server || true
+docker pull 992382743841.dkr.ecr.ap-northeast-2.amazonaws.com/squadus-server:latest
+docker run -d --name squadus-server -p 8080:8080 992382743841.dkr.ecr.ap-northeast-2.amazonaws.com/squadus-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
