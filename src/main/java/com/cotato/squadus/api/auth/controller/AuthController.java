631e4dd3825152e30fe6258e9e8a60736d720ebb
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-07-07 12:33:44 +0900

    Refactor: 패키지 구조 변경

---
+++ src/main/java/com/cotato/squadus/api/auth/controller/AuthController.java
@@ -0,0 +1,80 @@
+package com.cotato.squadus.api.auth.controller;
+
+import com.cotato.squadus.domain.auth.service.AuthService;
+import com.cotato.squadus.api.auth.dto.JoinRequest;
+import com.cotato.squadus.dto.LoginRequest;
+import com.cotato.squadus.entity.Member;
+import com.cotato.squadus.entity.RefreshEntity;
+import com.cotato.squadus.jwt.JWTUtil;
+import com.cotato.squadus.repository.MemberRepository;
+import com.cotato.squadus.repository.RefreshRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    private final JWTUtil jwtUtil;
+    private final MemberRepository memberRepository;
+    private final RefreshRepository refreshRepository;
+
+    @PostMapping("/join")
+    public ResponseEntity<Void> joinProcess(@RequestBody JoinRequest joinRequest) {
+        log.info("회원가입 컨트롤러 : {}, {}", joinRequest.getEmail(), joinRequest.getUsername());
+        authService.joinProcess(joinRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        Member member = memberRepository.findByUsername(loginRequest.getUsername());
+
+        if (member == null || !member.getEmail().equals(loginRequest.getEmail())) {
+            return ResponseEntity.status(401).build();
+        }
+
+        String username = member.getUsername();
+        String role = member.getRole();
+        System.out.println("role = " + role);
+
+        //토큰 생성
+        String access = jwtUtil.createJwt("access", username, role, 600000L);
+        String refresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
+
+        addRefreshEntity(username, refresh, 86400000L);
+
+        //응답 설정
+        response.setHeader("access", access);
+        response.setHeader("refresh", refresh);
+        response.setStatus(HttpStatus.OK.value());
+//        LoginResponse loginResponse = new LoginResponse(access, refresh);
+        return ResponseEntity.ok("Login Success");
+    }
+
+    private void addRefreshEntity(String username, String refresh, Long expiredMs) {
+
+        Date date = new Date(System.currentTimeMillis() + expiredMs);
+
+        RefreshEntity refreshEntity = new RefreshEntity();
+        refreshEntity.setUsername(username);
+        refreshEntity.setRefresh(refresh);
+        refreshEntity.setExpiration(date.toString());
+
+        refreshRepository.save(refreshEntity);
+    }
+}
