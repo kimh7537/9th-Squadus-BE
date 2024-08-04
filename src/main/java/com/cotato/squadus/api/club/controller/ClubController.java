198f1b8f33e616215baf7a8f530c18102b77d4db
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-08-04 16:18:25 +0900

    Chore: 동아리 관련 api swagger 설정

--- src/main/java/com/cotato/squadus/api/club/controller/ClubController.java
+++ src/main/java/com/cotato/squadus/api/club/controller/ClubController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "동아리", description = "동아리 관련 API")
 @RestController
 @RequestMapping("/v1/api/clubs")
 @RequiredArgsConstructor
@@ -19,12 +20,14 @@ public class ClubController {
     private final ClubService clubService;
 
     @PostMapping()
+    @Operation(summary = "동아리 생성", description = "동아리에 대한 정보를 바탕으로 동아리를 생성합니다")
     public ResponseEntity<ClubCreateResponse> createClub(@RequestBody ClubCreateRequest clubCreateRequest) {
         ClubCreateResponse clubCreateResponse = clubService.createClub(clubCreateRequest);
         return ResponseEntity.ok(clubCreateResponse);
     }
 
     @PostMapping("/{clubId}")
+    @Operation(summary = "동아리 가입 신청", description = "clubId와 동아리 가입에 대한 정보를 바탕으로 동아리 가입을 신청합니다")
     public ResponseEntity<ClubApplyResponse> joinClub(@PathVariable Long clubId, @RequestBody ClubApplyRequest clubApplyRequest) {
         ClubApplyResponse clubApplyResponse = clubService.joinClub(clubId, clubApplyRequest);
         return ResponseEntity.ok(clubApplyResponse);
