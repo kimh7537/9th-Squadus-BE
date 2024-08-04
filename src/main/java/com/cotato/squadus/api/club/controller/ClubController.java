61f3c1b19df1be3469e29b0e0980286db9572754
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-08-04 15:23:48 +0900

    Chore: 동아리 관련 api swagger 설정

--- src/main/java/com/cotato/squadus/api/club/controller/ClubController.java
+++ src/main/java/com/cotato/squadus/api/club/controller/ClubController.java
@@ -5,6 +5,8 @@ import com.cotato.squadus.api.club.dto.ClubCreateResponse;
 import com.cotato.squadus.api.club.dto.ClubApplyRequest;
 import com.cotato.squadus.api.club.dto.ClubApplyResponse;
 import com.cotato.squadus.domain.club.common.service.ClubService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "동아리", description = "동아리 관련 API")
 @RestController
 @RequestMapping("/v1/api/clubs")
 @RequiredArgsConstructor
@@ -20,12 +23,14 @@ public class ClubController {
     private final ClubService clubService;
 
     @PostMapping("/create")
+    @Operation(summary = "동아리 생성", description = "동아리에 대한 정보를 통해 동아리를 생성합니다")
     public ResponseEntity<ClubCreateResponse> createClub(@RequestBody ClubCreateRequest clubCreateRequest) {
         ClubCreateResponse clubCreateResponse = clubService.createClub(clubCreateRequest);
         return ResponseEntity.ok(clubCreateResponse);
     }
 
     @PostMapping("/apply")
+    @Operation(summary = "동아리 가입 신청", description = "유저의 id, 동아리의 id를 통해 동아리에 가입을 신청합니다.")
     public ResponseEntity<ClubApplyResponse> joinClub(@RequestBody ClubApplyRequest clubApplyRequest) {
         ClubApplyResponse clubApplyResponse = clubService.joinClub(clubApplyRequest);
         return ResponseEntity.ok(clubApplyResponse);
