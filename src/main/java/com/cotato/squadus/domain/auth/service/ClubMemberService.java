e0b0ae3a6c04471fc6100b9b6fca7e7037fcca0a
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-08-06 17:05:33 +0900

    Refactor: 동아리 공지 기능에서 동아리 회원 검증 기능 추가

--- src/main/java/com/cotato/squadus/domain/auth/service/ClubMemberService.java
+++ src/main/java/com/cotato/squadus/domain/auth/service/ClubMemberService.java
@@ -1,6 +1,8 @@
 package com.cotato.squadus.domain.auth.service;
 
 import com.cotato.squadus.common.config.auth.CustomOAuth2Member;
+import com.cotato.squadus.common.error.ErrorCode;
+import com.cotato.squadus.common.error.exception.AppException;
 import com.cotato.squadus.domain.auth.entity.Member;
 import com.cotato.squadus.domain.club.common.entity.ClubMember;
 import com.cotato.squadus.domain.club.common.repository.ClubMemberRepository;
@@ -21,11 +23,21 @@ public class ClubMemberService {
     private final MemberService memberService;
 
 
-    public ClubMember findClubMemberBySecurityContextHolder() {
+    // 임시 세션 정보를 통해 회원이 속한 동아리인지 검증
+    public void validateClubMember(Long clubId) {
+        ClubMember clubMember = findClubMemberBySecurityContextHolder();
+        log.info("현재 로그인 된 회원의 clubId: {} ", clubMember.getClub().getClubId());
+        if(!clubMember.getClub().getClubId().equals(clubId)) {
+            throw new AppException(ErrorCode.CLUB_ACCESS_DENIED);
+        }
+    }
+
+     private ClubMember findClubMemberBySecurityContextHolder() {
         CustomOAuth2Member oAuth2Member = (CustomOAuth2Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Member member = memberService.findMemberByUniqueId(oAuth2Member.getUniqueId());
         ClubMember clubMember = clubMemberRepository.findClubMemberByMember_MemberIdx(member.getMemberIdx())
                 .orElseThrow(() -> new EntityNotFoundException("해당 회원 고유번호를 가진 동아리 회원을 찾을 수 없습니다."));
         return clubMember;
     }
+
 }
