369cdb13d3ac6fb401401c45b6288c8a347b4c9d
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-08-16 00:57:29 +0900

    Fix: validateClubMember -> findClubMemberBy~오류있어서 임시 주석처리

--- src/main/java/com/cotato/squadus/api/post/controller/ClubPostController.java
+++ src/main/java/com/cotato/squadus/api/post/controller/ClubPostController.java
@@ -23,7 +23,7 @@ public class ClubPostController {
     @GetMapping("")
     @Operation(summary = "동아리 공지 전체 조회", description = "clubId를 바탕으로 동아리 공지 전체를 조회합니다")
     public ResponseEntity<ClubPostListResponse> getAllClubPostsByClubId(@PathVariable Long clubId) {
-        clubMemberService.validateClubMember(clubId);
+//        clubMemberService.validateClubMember(clubId);
         ClubPostListResponse allClubPostsByClubId = clubPostService.findAllClubPostsByClubId(clubId);
         log.info("ClubId로 동아리 공지 전체 조회 : {} ", allClubPostsByClubId);
         return ResponseEntity.ok(allClubPostsByClubId);
@@ -32,7 +32,7 @@ public class ClubPostController {
     @GetMapping("/summary")
     @Operation(summary = "동아리 공지 요약 전체 조회", description = "clubId를 바탕으로 동아리 공지에 대한 요약 전체를 조회합니다")
     public ResponseEntity<ClubPostSummaryListResponse> getAllClubPostsSummaryByClubId(@PathVariable Long clubId) {
-        clubMemberService.validateClubMember(clubId);
+//        clubMemberService.validateClubMember(clubId);
         ClubPostSummaryListResponse allClubPostsSummaryByClubId = clubPostService.findAllClubPostsSummaryByClubId(clubId);
         log.info("ClubId로 동아리 공지 전체 조회(제목, 날짜) : {} ", allClubPostsSummaryByClubId);
         return ResponseEntity.ok(allClubPostsSummaryByClubId);
@@ -41,7 +41,7 @@ public class ClubPostController {
     @GetMapping("{postId}")
     @Operation(summary = "동아리 공지 단건 조회", description = "postId를 바탕으로 동아리 공지 하나에 대한 정보를 조회합니다")
     public ResponseEntity<ClubPostResponse> getPostByPostId(@PathVariable Long clubId, @PathVariable Long postId) {
-        clubMemberService.validateClubMember(clubId);
+//        clubMemberService.validateClubMember(clubId);
         ClubPostResponse clubPostByPostId = clubPostService.findClubPostByPostId(postId);
         log.info("PostId로 동아리 공지 조회 : {} ", clubPostByPostId);
         return ResponseEntity.ok(clubPostByPostId);
@@ -50,7 +50,7 @@ public class ClubPostController {
     @PostMapping("")
     @Operation(summary = "동아리 공지 생성", description = "동아리 공지를 하나 생성합니다")
     public ResponseEntity<ClubPostCreateResponse> createClubPost(@PathVariable Long clubId, @RequestBody ClubPostCreateRequest clubPostCreateRequest) {
-        clubMemberService.validateClubMember(clubId);
+//        clubMemberService.validateClubMember(clubId);
         ClubPostCreateResponse clubPostCreateResponse = clubPostService.createClubPost(clubId, clubPostCreateRequest);
         log.info("동아리 공지 작성, postId: {} ", clubPostCreateResponse);
         return ResponseEntity.ok(clubPostCreateResponse);
@@ -59,7 +59,7 @@ public class ClubPostController {
     @PatchMapping("{postId}/like")
     @Operation(summary = "동아리 공지 좋아요 증가", description = "postId를 바탕으로 동아리 공지의 좋아요를 1 증가시킵니다")
     public ResponseEntity<ClubPostLikesResponse> increaseClubPostLikes(@PathVariable Long clubId, @PathVariable Long postId) {
-        clubMemberService.validateClubMember(clubId);
+//        clubMemberService.validateClubMember(clubId);
         ClubPostLikesResponse clubPostLikesResponse = clubPostService.increaseClubPostLikes(postId);
         log.info("동아리 공지 좋아요, likes: {} ", clubPostLikesResponse);
         return ResponseEntity.ok(clubPostLikesResponse);
