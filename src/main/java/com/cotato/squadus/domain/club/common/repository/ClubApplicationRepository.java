237cf5531239d9231fc7fcd03f63c0f18f556128
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-08-23 04:11:49 +0900

    Feat: 동아리 가입 거절 기능, 동아리 가입신청 내역 조회 기능 구현

--- src/main/java/com/cotato/squadus/domain/club/common/repository/ClubApplicationRepository.java
+++ src/main/java/com/cotato/squadus/domain/club/common/repository/ClubApplicationRepository.java
@@ -3,6 +3,9 @@ package com.cotato.squadus.domain.club.common.repository;
 import com.cotato.squadus.domain.club.common.entity.ClubApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ClubApplicationRepository extends JpaRepository<ClubApplication, Long> {
 
+    List<ClubApplication> findByRecruitingPost_PostId(Long postId);
 }
